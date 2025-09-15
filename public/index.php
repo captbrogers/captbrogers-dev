909ac938b4a6862035ca9ad3682173c139b94f3c
Brian Rogers <captbrogers@protonmail.ch>
2025-09-14 18:54:53 -0600

    updating to use three PHP files

--- public/index.php
+++ public/index.php
@@ -1,1 +1,10 @@
-<?php echo "greetings program"; ?>
+<?php include('../resources/templates/header.php'); ?>
+                    <div class="mugshot-container">
+                        <img src="/images/avatar.jpg" alt="He's a guy that does stuff" width="240px" height="640px" class="float-left mr-6">
+                        <div class="about-conatiner">
+                            <p>Greetings! My name is Brian. I've got an interesting history when it comes to programming. It all started with learning BASIC on a Texas Instruments calculator to give me answers for formula problems when I was in middle school. It moved on to learning C/C++ in high school using a "For Dummies" book. Not long after I graduated I was doing tech support for a web hosting company and teaching myself everything web related while also attending college in the evenings. I still remember the days of having to do nested div tags to get rounded corners!</p>
+                            <p>For the last decade I have worked with WordPress and Laravel extensively and moved on from jQuery to using VueJS. I've been using Linux to self-host many things and also use it as my desktop and laptop OS of choice.</p>
+                            <p>I'm available for all kinds of work right now, so feel free to reach out to me.</p>
+                        </div>
+                    </div>
+<?php include('../resources/templates/footer.php'); ?>
---
+++ public/services.php
@@ -0,0 +1,5 @@
+<?php include('../resources/templates/header.php'); ?>
+<div>
+Here is the list of services I can provide.
+</div>
+<?php include('../resources/templates/footer.php'); ?>
