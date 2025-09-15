b003b88020f2168c978770bdddbe72648057abda
Brian Rogers <captbrogers@protonmail.ch>
2025-09-14 19:20:47 -0600

    updated the services page

--- public/services.php
+++ public/services.php
@@ -1,5 +1,6 @@
 <?php include('../resources/templates/header.php'); ?>
-<div>
-Here is the list of services I can provide.
+<div class="services-container">
+    <p>Do you need help in the form of full time employment? I'm here for that. What about part time? I can do that too. I can do contract work as well, I'd love to help by coming in as a consultant to solve a specific problem. I can assist you with planning and executing a process to update an old codebase, implement automated testing, or finding the right combination for your review process.</p>
+    <p>If you are not a recruiter, please feel free to reach out to me for anything related to Laravel and Vue. I'm open to considering available opportunities.</p>
 </div>
 <?php include('../resources/templates/footer.php'); ?>
