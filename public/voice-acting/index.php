12213ebbe7decdc2f60f6d2a206162effc2815e2
Brian Rogers <captbrogers@protonmail.ch>
2025-09-23 13:59:57 -0600

    updating voice acting page with placeholder

--- public/voice-acting/index.php
+++ public/voice-acting/index.php
@@ -1,4 +1,4 @@
-<?php $domain = "https://captbrogers.dev"; ?><!DOCTYPE html>
+<?php $domain = "https://captbrogers.test"; ?><!DOCTYPE html>
 <html lang="en-US">
     <head>
         <meta charset="utf-8">
@@ -42,16 +42,36 @@
     <body class="font-sans antialiased">
         <div id="app" class="min-h-screen bg-stone-400">
             <div class="max-w-7xl mx-auto pt-24">
-                <p>You can reach me at captbrogers@protonmail.ch</p>
-
-                <div class="grid grid-flow-row gap-8">
+                <div class="bg-white p-6 rounded shadow-lg items-center justify-center flex">
+                    <p class="text-xl">You can reach me on <a class="text-link" href="https://www.acx.com/narrator?p=A2IHHXGQH9NOY5" title="ACX profile for Brian Rogers">ACX</a> or <a class="text-link" href="https://cstng.cc/captbrogers" title="Casting Call: Brian Rogers">Casting Call Club</a></p>
+                </div>
+                <div class="grid grid-flow-row gap-8 mt-8">
                     <div class="voice-sample-container">
                         <div class="controls">
-                            <div class="play-button"></div>
-                            <div class="pause-button"></div>
+                            <div class="play-button">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-play-icon lucide-circle-play"><path d="M9 9.003a1 1 0 0 1 1.517-.859l4.997 2.997a1 1 0 0 1 0 1.718l-4.997 2.997A1 1 0 0 1 9 14.996z"/><circle cx="12" cy="12" r="10"/></svg>
+                            </div>
+                            <div class="pause-button">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-octagon-pause-icon lucide-octagon-pause"><path d="M10 15V9"/><path d="M14 15V9"/><path d="M2.586 16.726A2 2 0 0 1 2 15.312V8.688a2 2 0 0 1 .586-1.414l4.688-4.688A2 2 0 0 1 8.688 2h6.624a2 2 0 0 1 1.414.586l4.688 4.688A2 2 0 0 1 22 8.688v6.624a2 2 0 0 1-.586 1.414l-4.688 4.688a2 2 0 0 1-1.414.586H8.688a2 2 0 0 1-1.414-.586z"/></svg>
+                            </div>
                         </div>
                         <div class="info-container">
-                            <
+                            <p class="font-bold text-xl">Example Snippet</p>
+                            <p>From the book: Super Amazing Adventures of Pudding</p>
+                            <div class="flex items-center space-x-8">
+                                <div class="flex items-center space-x-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clock-icon lucide-clock"><path d="M12 6v6l4 2"/><circle cx="12" cy="12" r="10"/></svg>
+                                    <div>0:00</div>
+                                </div>
+                                <div class="flex items-center space-x-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-user-round-icon lucide-circle-user-round"><path d="M18 20a6 6 0 0 0-12 0"/><circle cx="12" cy="10" r="4"/><circle cx="12" cy="12" r="10"/></svg>
+                                    <div>Male</div>
+                                </div>
+                                <div class="flex items-center space-x-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book-open-text-icon lucide-book-open-text"><path d="M12 7v14"/><path d="M16 12h2"/><path d="M16 8h2"/><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"/><path d="M6 12h2"/><path d="M6 8h2"/></svg>
+                                    <div>Fantasy, Sci-Fi</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
